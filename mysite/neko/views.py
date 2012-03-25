2cbc4b8ac94ebd3a42ae8282afd8a42df4382091
shullidrake <nozomisumiya@gmail.com>
2012-03-24 22:25:49 -0700

    adding base template file

--- mysite/neko/views.py
+++ mysite/neko/views.py
@@ -1,7 +1,8 @@
 # Copyright (C) 2012 Nozomi Lipps
 """Views functions"""
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
 
 
 def home(request):
-    return HttpResponse("Hello World!")
+    return render_to_response('home.html')
