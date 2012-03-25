08f42ddb3d0e2611c3fb61cd19094038c2049e21
shullidrake <nozomisumiya@gmail.com>
2012-03-25 11:35:19 -0700

    use jquey

--- mysite/neko/views.py
+++ mysite/neko/views.py
@@ -2,7 +2,8 @@
 """Views functions"""
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
+from django.template import RequestContext
 
 
 def home(request):
-    return render_to_response('home.html')
+    return render_to_response('home.html', context_instance=RequestContext(request))
