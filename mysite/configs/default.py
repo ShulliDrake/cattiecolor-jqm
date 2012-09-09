4e6ce90f9eb7ef0fc7962556e9253300a9886e58
nozomi <nozomisumiya@gmail.com>
2012-09-08 20:14:07 -0700

    Add more colors

--- mysite/configs/default.py
+++ mysite/configs/default.py
@@ -5,32 +5,40 @@ TIE_COLORS = [
     OrderedDict(
         [('#000', 'Black'),
          ('#423732', 'Gunmetal'),
-         ('#C0C0C0', 'Silver')]),
+         ('#C0C0C0', 'Silver'),
+         ('#F5F5F5', 'White Smoke')]),
     OrderedDict(
         [('#151B54', 'Navy'),
          ('#151B8D', 'Blue'),
+         ('#1034A6', 'Egyptian'),
          ('#0F52BA', 'Sapphire')]),
     OrderedDict(
         [('#007FFF', 'Azure'),
          ('#82CAFF', 'Sky Blue'),
-         ('#B2FFFF', 'Celeste'),]),
+         ('#B2FFFF', 'Celeste'),
+         ('#F0F8FF', 'Alice Blue')]),
     OrderedDict(
         [('#355E3B', 'Hunter'),
          ('#009E60', 'Shamrock'),
-         ('#32CD32', 'Lime')]),
+         ('#32CD32', 'Lime'),
+         ('#ADFF2F', 'Green-yellow')]),
     OrderedDict(
         [('#7B3F00', 'Chocolate'),
          ('#6F4E37', 'Coffee'),
-         ('#A0785A', 'Chamoisee')]),
+         ('#A0785A', 'Chamoisee'),
+         ('#C19A6B', 'Fallow')]),
     OrderedDict(
         [('#FF7518', 'Pumpkin'),
          ('#FF7F50', 'Coral'),
-         ('#FFBF00', 'Amber')]),
+         ('#FFBF00', 'Amber'),
+         ('#FBCEB1', 'Apricot')]),
     OrderedDict(
         [('#FBEC5D', 'Maize'),
          ('#FFFF66', 'Light Yellow'),
-         ('#FFFDD0', 'Creme')]),
+         ('#FFFDD0', 'Cream'),
+         ('#FFFFF0', 'Ivory')]),
     OrderedDict(
         [('#E0115F', 'Ruby'),
          ('#FF69B4', 'Hot Pink'),
-         ('#FFA6C9', 'Carnation')])]
+         ('#FFA6C9', 'Carnation'),
+         ('#FBAED2', 'Lavender')])]
